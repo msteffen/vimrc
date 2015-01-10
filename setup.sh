6bca08c88b3929bf962e341f4b68d7c0904eb4a7
Matthew Steffen <matthew.steffen@gmail.com>
2015-01-10 15:43:51 -0500

    Use temp files in setup.sh, since vim -c didn't seem to work for some reason

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+# tempfile with vimscript for installing Vundle, etc
+tf=$(mktemp -p.)
+printf "PluginInstall\nqa" >${tf}
+
+# Copy Vundle & create YCM directory
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -c "PluginInstall | qa"
+# for some reason, vim -c "PluginInstall | qa" doesn't seem to do the right
+# thing. If I can figure out how to fix it, then fix it (tempfiles are gross)
+vim -S ${tf}
+
+# Install YCM
 sudo apt-get install build-essential cmake python-dev
-cd ~/.vim/bundle/YouCompleteMe && ./install.sh
-vim -c "PluginInstall | qa"
+(cd ~/.vim/bundle/YouCompleteMe && ./install.sh)
+vim -S ${tf}
+
+rm ${tf}
