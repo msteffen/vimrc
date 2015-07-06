78d40b9c7833f8dd7fc44f2d06c8b4b3d0cc99dd
msteffen <matthew.steffen@gmail.com>
2015-07-06 01:44:36 -0700

    Update setup.sh
    Install plugins in vim (to create YouCompleteMe directory) *before* running YCM's install script

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install YCM
 sudo apt-get install build-essential cmake python-dev
+echo -e ":PluginInstall\n:q" | vim
 (cd ~/.vim/bundle/YouCompleteMe && ./install.sh)
-echo -e ":PluginInstall\n:q"
