23d507f9529e33d0b5589ab420f9d45903e571e6
msteffen <matthew.steffen@gmail.com>
2016-04-03 14:22:23 -0700

    Add flag so vim doesn't complain about stdin

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install YCM
 sudo apt-get install build-essential cmake python-dev
-echo -e ":PluginInstall\n:q" | vim
+echo -e ":PluginInstall\n:q" | vim -e # Starts vim in ex mode
+# I haven't really tried the above in a totally fresh install,
+# so maybe this works better:
+# vim -c ":PluginInstall" -c ":qa" (or something similar)
 (cd ~/.vim/bundle/YouCompleteMe && ./install.sh)
