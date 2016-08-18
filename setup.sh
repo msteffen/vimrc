05a80df625fbf832ee799662c9a9671d9d973bb5
msteffen <msteffen@pachyderm.io>
2016-08-18 12:55:23 -0700

    Update setup.sh
    Simplify install script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,5 @@
 
 # Copy Vundle & create YCM directory
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Install YCM
-sudo apt-get install build-essential cmake python-dev
-echo -e ":PluginInstall\n:q" | vim -e # Starts vim in ex mode
-# I haven't really tried the above in a totally fresh install,
-# so maybe this works better:
-# vim -c ":PluginInstall" -c ":qa" (or something similar)
-(cd ~/.vim/bundle/YouCompleteMe && ./install.sh)
+vim -c ":PluginInstall" -c ":qa"
+echo "See https://github.com/msteffen/vimrc/blob/master/README.md if you want to set up YouCompleteMe as well"
