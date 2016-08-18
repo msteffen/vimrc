189c10834d13f8571c45ef89af3c9ecd122c7013
msteffen <msteffen@pachyderm.io>
2016-08-18 12:57:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copy Vundle & create YCM directory
+curl https://raw.githubusercontent.com/msteffen/vimrc/master/vimrc >~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c ":PluginInstall" -c ":qa"
 echo "See https://github.com/msteffen/vimrc/blob/master/README.md if you want to set up YouCompleteMe as well"
