c421a3d7c2f8c2f1324132003c2a8d827c668a4b
msteffen <msteffen@pachyderm.io>
2016-09-02 14:16:48 -0700

    Echo progress

--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@
 which git || { sudo apt install git; }
 
 # Copy Vundle & create YCM directory
+echo "Downloading vimrc..."
 curl https://raw.githubusercontent.com/msteffen/vimrc/master/vimrc >~/.vimrc
+echo "Downloading vundle..."
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Installing plugins..."
 vim -c ":PluginInstall" -c ":qa"
 echo "See https://github.com/msteffen/vimrc/blob/master/README.md if you want to set up YouCompleteMe as well"
