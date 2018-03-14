bf2a2df49a2d3cf016049016347074787039f2d5
Matthew Steffen <msteffen@pachyderm.io>
2018-03-14 13:43:17 -0700

    make setup.sh accomodate vimrc variations

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ which git || { sudo apt install git; }
 
 # Copy Vundle & create YCM directory
 echo "Downloading vimrc..."
-curl https://raw.githubusercontent.com/msteffen/vimrc/master/vimrc >~/.vimrc
+curl https://raw.githubusercontent.com/msteffen/vimrc/master/${1:-vimrc} >~/.vimrc
 echo "Downloading vundle..."
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Installing plugins..."
