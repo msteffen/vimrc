30066e283a0fd6660c5972f97691a235a2705986
Matthew Steffen <m@tthew.io>
2024-08-21 15:54:47 -0700

    Fix for stupid go completion bug

--- setup.sh
+++ setup.sh
@@ -14,6 +14,12 @@ vim -c ":PluginInstall" -c ":GoInstallBinaries" -c ":qa"
 if [[ "${config}" =~ vimrc|vimrc_go_development ]]; then
   echo "Compiling YouCompleteMe with --go-completer (but not --clang-completer for C++)"
   cd ~/.vim/bundle/YouCompleteMe
+  # Really stupid fix. The version of ycm-core/ycmd that's referenced by the
+  # git submodule in the ycm-core/YouCompleteMe repo has a bug with go
+  # completion (it depends on a version of gopls that doesn't build :facepalm:
+  # The bug has been fixed in the latest version of the ycmd repo, but the
+  # submodule hasn't been updated :facepalm:.
+  git submodule update --checkout -- ./third_party/ycmd
   python3 ./install.py --go-completer
   cd -
 else
