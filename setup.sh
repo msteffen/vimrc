29b89a688099818fadda6ea34e9366eb418fdae6
Matthew Steffen <m@tthew.io>
2024-08-21 15:29:41 -0700

    Compile YCM inside setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,19 @@
 which git || { sudo apt install git; }
 
 # Copy Vundle & create YCM directory
+config="${1:-vimrc}"
 echo "Downloading vimrc..."
-curl https://raw.githubusercontent.com/msteffen/vimrc/master/${1:-vimrc} >~/.vimrc
+curl https://raw.githubusercontent.com/msteffen/vimrc/master/${config} >~/.vimrc
 echo "Downloading vundle..."
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Installing plugins..."
 vim -c ":PluginInstall" -c ":GoInstallBinaries" -c ":qa"
-echo "See https://github.com/msteffen/vimrc/blob/master/README.md if you want to set up YouCompleteMe as well"
+
+if [[ "${config}" =~ vimrc|vimrc_go_development ]]; then
+  echo "Compiling YouCompleteMe with --go-completer (but not --clang-completer for C++)"
+  cd ~/.vim/bundle/YouCompleteMe
+  python3 ./install.py --go-completer
+  cd -
+else
+  echo "See https://github.com/msteffen/vimrc/blob/master/README.md if you want to set up YouCompleteMe as well"
+fi
